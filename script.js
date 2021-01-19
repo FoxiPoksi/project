d738f84fdbc1660c8ce733184e01fdaf81266000
Ilya <Superteg31@gmail.com>
2021-01-19 20:09:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict'
+
+a = 5;
+console.log(a);
+
+// alert("Hello World!")
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object")
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2"=== 2);
+
+let isChecked = false,
+    isClose = false; 
+
+console.log(isChecked || !isClose);    
